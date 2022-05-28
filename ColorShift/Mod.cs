5b9bf124a88483902a2abef3a2451e5633ee1d75
Usman Shafiq <ushafiq141@gmail.com>
2022-05-28 09:42:30 -0400

    Work on Cursed

--- ColorShift/Mod.cs
+++ ColorShift/Mod.cs
@@ -100,6 +100,7 @@ namespace ColorShift
             mainMode.Value = mainFilter.mode;
             mainMode.Save();
 			category.SaveToFile();
+			//ResetCursed();
 			LoggerInstance.Msg("Filter Mode: " + mainFilter.mode);
 		}
 
@@ -110,6 +111,67 @@ namespace ColorShift
                 mainFilter.showDifference = showDifference.Value;
                 mainFilter.forceRefresh = true;
 			}
-		}        
+		}
+
+
+        // BPM 165
+        // Each beat lasts 4/11 of a second (60/165)
+		/*
+        static float lengthOfBeat = (4f / 11f) * 2f;
+		static float timeElapsed = 0f;
+		static int colorArrayPos = 0;
+
+
+		// Colors:
+		public static Color[][] Cursed = new Color[3][]
+		{
+			new Color[] {
+				new Color(0.7f,0f,0f),   new Color(0f,0.7f,0f), new Color(1f,1f,1f)
+			},
+			new Color[] {
+				new Color(0.7f,0f,0f),   new Color(1f,1f,1f), new Color(0f,0f,0.7f)
+			},
+			new Color[] {
+				new Color(1f,1f,1f),   new Color(0f,0.7f,0f), new Color(0f,0f,0.7f)
+			},
+		};
+
+		//CurColor
+		public static Color[] curColor = new Color[] { new Color(1f, 0f, 0f), new Color(0f, 1f, 0f), new Color(0f, 0f, 1f) };
+
+		private static void ResetCursed()
+        {
+			timeElapsed = 0f;
+			colorArrayPos = 0;
+		}
+
+        public override void OnUpdate()
+        {
+            if(mainMode.Value == Mode.Custom5)
+            {
+                if(timeElapsed < lengthOfBeat)
+                {
+                    curColor[0] = Color.Lerp(curColor[0], Cursed[colorArrayPos][0], timeElapsed / lengthOfBeat);
+					curColor[1] = Color.Lerp(curColor[1], Cursed[colorArrayPos][1], timeElapsed / lengthOfBeat);
+					curColor[2] = Color.Lerp(curColor[2], Cursed[colorArrayPos][2], timeElapsed / lengthOfBeat);
+					timeElapsed += Time.deltaTime;
+				}
+                else
+                {
+                    timeElapsed = 0f;
+					colorArrayPos++;
+					if (colorArrayPos > Cursed.Length - 1)
+					{
+						colorArrayPos = 0;
+					}
+				}
+
+				Filter.RGB[13] = curColor;
+				RefreshMainFilter();
+
+                timeElapsed += Time.deltaTime;
+            }
+        }
+		*/
 	}
 }
